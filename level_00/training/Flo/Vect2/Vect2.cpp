6b1e02ef47d9d76d53906a5f8853f9f7e65a7cf8
Floriano Albertini <floriano.webdev@gmail.com>
2025-06-25 15:26:58 +0200

    backup

--- level_00/training/Flo/Vect2/Vect2.cpp
+++ level_00/training/Flo/Vect2/Vect2.cpp
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:37:52 by falberti          #+#    #+#             */
-/*   Updated: 2025/06/25 15:02:29 by falberti         ###   ########.fr       */
+/*   Updated: 2025/06/25 15:26:37 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,38 @@ int vect2::operator[](const int pos) const{
 int& vect2::operator[](const int pos){
     return (pos == 0) ? _x : _y;
 }
+
+vect2 vect2::operator+(int num) const{
+    return vect2(_x + num, _y + num);
+}
+vect2 vect2::operator+(const vect2& vec) const{
+    return vect2(_x + vec._x, _y + vec._y);
+}
+
+vect2 vect2::operator-(int num) const{
+    return vect2(_x - num, _y - num);
+}
+vect2 vect2::operator-(const vect2& vec) const{
+    return vect2(_x - vec._x, _y - vec._y);
+}
+vect2 vect2::operator*(int num) const{
+    return vect2(_x * num, _y * num);
+}
+vect2 vect2::operator*(const vect2& vec) const{
+    return vect2(_x * vec._x, _y * vec._y);
+}
+
+vect2 operator+(int num, const vect2& vec){
+    return vect2(vec[0] + num, vec[1] + num);
+}
+vect2 operator-(int num, const vect2& vec){
+    return vect2(num - vec[0], num - vec[1]);
+}
+vect2 operator*(int num, const vect2& vec){
+    return vect2(vec[0] * num, vec[1] * num);
+}
+
+
 
 void vect2::print(std::ostream& os) const{
     os << "{" << _x << ", " << _y << "}" << std::endl;
--- level_00/training/Flo/Vect2/Vect2.hpp
+++ level_00/training/Flo/Vect2/Vect2.hpp
@@ -6,7 +6,7 @@
 /*   By: falberti <falberti@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/25 14:31:37 by falberti          #+#    #+#             */
-/*   Updated: 2025/06/25 14:58:13 by falberti         ###   ########.fr       */
+/*   Updated: 2025/06/25 15:23:18 by falberti         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,8 +28,18 @@ class vect2 {
         int operator[](int pos) const;
         int& operator[](const int pos);
 
+        vect2 operator+(int num) const;
+        vect2 operator+(const vect2& vec) const;
+        vect2 operator-(int num) const;
+        vect2 operator-(const vect2& vec) const;
+        vect2 operator*(int num) const;
+        vect2 operator*(const vect2& vec) const;
+
         void print(std::ostream& os) const;
         friend std::ostream& operator<<(std::ostream& os, const vect2& vec);
+        friend vect2 operator+(int num, const vect2& vec);
+        friend vect2 operator-(int num, const vect2& vec);
+        friend vect2 operator*(int num, const vect2& vec);
 
 };
 
