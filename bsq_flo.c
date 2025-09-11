b5bf7a2a6fa05825849afa8b3de6eb2c1ce81959
Floriano Albertini <floriano.webdev@gmail.com>
2025-09-11 17:45:17 +0200

    backup

--- bsq_flo.c
+++ bsq_flo.c
@@ -9,6 +9,10 @@ typedef struct s_map {
     char **map;
 } t_map;
 
+int min3(int a, int b, int c){
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+}
+
 t_map *read_map(char *filename){
     FILE *file;
     if(!filename){
@@ -60,6 +64,47 @@ t_map *read_map(char *filename){
     return map;
 }
 
+void solve_bsq(t_map *map){
+    int **dp = malloc(map->height * sizeof(int*));
+    for (int i = 0; i < map->height; i++){
+        dp[i] = malloc(sizeof(int) * map->width);
+    }
+
+    int max_size = 0, best_i = 0, best_y = 0;
+
+    for (int i = 0; i < map->height; i++){
+        for (int y = 0; y < map->width; y++){
+            if(map->map[i][y] == map->obstacle){
+                dp[i][y] = 0;
+            } else if (i == 0 || y == 0){
+                dp[i][y] = 1;
+            } else {
+                dp[i][y] = min3(dp[i - 1][y], dp[i][y - 1], dp[i -1][y -1]) + 1;
+            }
+
+            if (dp[i][y] > max_size){
+                max_size = dp[i][y];
+                best_i = i;
+                best_y = y; 
+            }
+        }
+    }
+
+    int start_i = best_i - max_size + 1;
+    int start_y = best_y - max_size + 1;
+    for (int i = start_i; i < start_i + max_size; i++){
+        for (int y = start_y; y < start_y + max_size; y++){
+            map->map[i][y] = map->full;
+        }
+    }
+
+    for (int i = 0; i < map->height; i++){
+        free(dp[i]);
+    }
+    free(dp);
+
+}
+
 int is_valid(t_map *map){
     if(map->height <= 0 || map->width <= 0) return 0;
     if (map->empty == map->obstacle || map->empty == map->full || map->full == map->obstacle) return 0;
@@ -91,10 +136,13 @@ void process_file(char *filename){
         if (map) free_map(map);
         return ;
     }
-    //resolve_bsq(map);
     print_map(map);
-    free_map(map);
     fprintf(stdout, "\n");
+    solve_bsq(map);
+    print_map(map);
+    fprintf(stdout, "\n");
+    free_map(map);
+    
 }
 
 int main(int ac,  char **av){
