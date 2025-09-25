6406d08cb22c49016b00ea9287b5b78b14a587eb
Floriano Albertini <floriano@Florianos-MacBook-Pro.local>
2025-09-25 10:33:00 +0200

    backup

--- bsq_flo3.c
+++ bsq_flo3.c
@@ -16,6 +16,10 @@ int ft_strlen(char *str){
     return count;
 }
 
+int min3(int a, int b, int c){
+    return (a < b) ? ((a < c) ? a : c) : ((b < c) ? b : c);
+}
+
 void print_map(t_map *map){
     for(int i = 0; i < map->height; i++){
         fprintf(stdout, "%s\n", map->map[i]);
@@ -93,6 +97,51 @@ t_map *parse_map(char *filename){
     return map;
 }
 
+void solve_bsq(t_map *map){
+    //alloce mem
+    int **dp = malloc(sizeof(int*) * map->height);
+    if (dp == NULL) return ;
+    for (int y = 0; y < map->height; y++){
+        dp[y] = malloc(sizeof(int) * map->width);
+        if (dp[y] == NULL) return ;
+    }
+
+    int max_size = 0, best_y = 0, best_x = 0;
+
+    for (int y = 0; y < map->height; y++){
+        for (int x = 0; x < map->width; x++){
+            if(map->map[y][x] == map->obst){
+                dp[y][x] = 0;
+            } else if (y == 0 || x == 0){
+                dp[y][x] = 1;
+            } else {
+                dp[y][x] = min3(dp[y -1][x], dp[y][x -1], dp[y -1][x -1]) + 1;
+            }
+
+            if(dp[y][x] > max_size){
+                max_size = dp[y][x];
+                best_y = y;
+                best_x = x;
+            }
+        }
+    }
+
+    int start_y = best_y - max_size + 1;
+    int start_x = best_x - max_size + 1;
+
+    for (int y = start_y; y < start_y + max_size; y++){
+        for(int x = start_x; x < start_x + max_size; x++){
+            map->map[y][x] = map->full; 
+        }
+    }
+
+    //free meme
+    for (int y = 0; y < map->height; y++){
+        free(dp[y]);
+    }
+    free(dp);
+}
+
 void process(char *filename){
     t_map *map = parse_map(filename);
     if (!map || !validate(map)){
@@ -100,7 +149,7 @@ void process(char *filename){
         fprintf(stderr, "map error\n");
         return ;
     }
-    //solve_bsq(map);
+    solve_bsq(map);
     print_map(map);
     free_map(map);
 }
